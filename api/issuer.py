d7d7991bdc7467ff05fe996592d8ca735b57f526
Dennis Wendland <dennis.wendland@fiware.org>
2023-05-31 10:47:33 +0200

    Implementing route for creating issuer

--- api/issuer.py
+++ api/issuer.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response, current_app, abort, request, redirect
 from api.util.issuer_handler import extract_access_token, get_samedevice_redirect_url
+from api.util.issuer_handler import decode_token_with_jwk, check_create_role, forward_til_request
 import time
 
 from api.exceptions.issuer_exception import IssuerException
@@ -9,9 +10,10 @@ from api.exceptions.database_exception import DatabaseException
 issuer_endpoint = Blueprint("issuer_endpoint", __name__)
 
 # POST /issuer
+# TODO: Check for PUT to overwrite by DID: https://github.com/FIWARE/trusted-issuers-list/blob/main/api/trusted-issuers-list.yaml#L51
 @issuer_endpoint.route("/issuer", methods = ['POST'])
 def index():
-    current_app.logger.debug('Received request at /issuer endpoint')
+    current_app.logger.debug('Received request at /issuer endpoint (HTTP Method: {})'.format(request.method))
 
     # Load config
     conf = current_app.config['as']
@@ -41,9 +43,48 @@ def index():
     # Received JWT in Authorization header
     current_app.logger.debug("...received access token JWT in incoming request: {}".format(request_token))
     
-    # validate JWT with verifier JWKS
-    # check role --> define credential and role first
-    # forward request to TIL /issuer endpoint
-    # return TIL response
+    # Validate JWT with verifier JWKS
+    payload = None
+    try:
+        current_app.logger.debug("... validating and decoding JWT using JWKS ...")
+        payload = decode_token_with_jwk(request_token, conf)
+    except IssuerException as die:
+            current_app.logger.debug("Error when validating/decoding: {}. Returning status {}.".format(die.internal_msg, die.status_code))
+            abort(die.status_code, die.public_msg)
+    current_app.logger.debug("... decoded token payload: {}".format(payload))
 
-    return "OK", 200
+    # Check TIL access depending on HTTP method
+    if request.method == 'POST':
+        # POST: Create issuer flow
+        
+        # Check for 'Create Issuer' role
+        try:
+            current_app.logger.debug("... checking for necessary role to create issuer")
+            if not check_create_role(payload, conf):
+                current_app.logger.debug("Required role was not found in JWT credential. Returning status 401.")
+                current_app.logger.debug("... required role '{}' for target DID '{}'".format(conf['issuer']['roles']['createRole'], conf['issuer']['providerId']))
+                abort(401, "Issued roles do not allow to create an issuer")
+        except IssuerException as cie:
+            current_app.logger.debug("Error when checking for required role: {}. Returning status {}.".format(cie.internal_msg, cie.status_code))
+            abort(cie.status_code, cie.public_msg)
+        current_app.logger.debug("... access granted!")
+        
+    elif request.method == 'PUT':
+        # PUT: Update issuer flow
+    
+        # TODO: Implement issuer update
+        abort(500, "PUT not implemented")
+    else:
+        # should not happen
+        abort(500, "Invalid HTTP method")
+        
+    # Forward request to TIL
+    try:
+        current_app.logger.debug("... forwarding request to TIL")
+        res = forward_til_request(request, conf)
+        current_app.logger.debug("... received TIL response (code: {}): {}".format(res.status_code, res.content))
+        current_app.logger.debug("... returning response to sender!")
+        return res.content, res.status_code, res.headers.items()
+    except IssuerException as fie:
+        current_app.logger.error("Error when forwarding request to TIL: {}. Returning status {}.".format(fie.internal_msg, fie.status_code))
+        abort(fie.status_code, fie.public_msg)
--- api/util/issuer_handler.py
+++ api/util/issuer_handler.py
@@ -1,6 +1,7 @@
 import requests
 import time, os
 import jwt
+from jwt import PyJWKClient
 import uuid
 from requests.exceptions import HTTPError
 
@@ -56,3 +57,135 @@ def get_samedevice_redirect_url(conf):
 
     # Build URL
     return "{}{}?state={}&client_id={}".format(verifier_uri, samedevice_path, session_id, client_id)
+
+# Validate and decode the token using JWKS
+def decode_token_with_jwk(token, conf):
+
+    # Get JWKS URI and path
+    if not 'issuer' in conf or not 'verifierUri' in conf['issuer']:
+        message = "Missing 'verifierUri' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    verifier_uri = conf['issuer']['verifierUri']
+    if not 'issuer' in conf or not 'jwksPath' in conf['issuer']:
+        message = "Missing 'jwksPath' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    jwks_path = conf['issuer']['jwksPath']
+
+    # Get allowed algorithms
+    if not 'algorithms' in conf['issuer']:
+        message = "Missing 'algorithms' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    alg = conf['issuer']['algorithms']
+    if len(alg) < 1:
+        message = "Empty 'algorithms' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    
+    # Build JWKS URL
+    url = "{}{}".format(verifier_uri, jwks_path)
+
+    # JWK client
+    jwks_client = PyJWKClient(url)
+
+    # Get signing key from JWKS endpoint by kid specified in token
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+    except Exception as kex:
+        message = "Error when obtaining signing key"
+        int_msg = message + ": {}".format(kex)
+        raise IssuerException(message, int_msg, 400)
+
+    # Validate and decode
+    try:
+        data = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=alg,
+            options = {
+                "verify_aud": False
+            }
+        )
+    except Exception as dex:
+        message = "Invalid token provided"
+        int_msg = message + ": {}".format(dex)
+        raise IssuerException(message, int_msg, 401)
+
+    return data
+
+# Check if credential contains necessary role
+def check_role(credential_roles, required_role, provider_id):
+
+    # Loop over credential roles and look for required role
+    for r in credential_roles:
+        if 'target' in r and r['target'] == provider_id:
+            if 'names' in r['target']:
+                r_names = r['target']['names']
+                if required_role in r_names:
+                    return True
+
+    return False
+    
+
+# Check if credential contains necessary role to create issuer
+def check_create_role(token_payload, conf):
+
+    # Get required role
+    if not 'issuer' in conf or not 'roles' in conf['issuer']:
+        message = "Missing 'roles' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    conf_roles = conf['issuer']['roles']
+    if not 'createRole' in conf_roles:
+        message = "Missing 'createRole' in issuer roles config"
+        raise IssuerException("Internal server error", message, 500)
+    create_role = conf_roles['createRole']
+
+    # Get target DID
+    if not 'providerId' in conf['issuer']:
+        message = "Missing 'providerId' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    provider_id = conf['issuer']['providerId']
+
+    # Get roles from credential payload
+    if not 'verifiableCredential' in token_payload:
+        msg = "No 'verifiableCredential' in JWT"
+        raise IssuerException(msg, None, 400)
+    credential = token_payload['verifiableCredential']
+    if not 'credentialSubject' in credential:
+        msg = "No 'credentialSubject' in verifiableCredential"
+        raise IssuerException(msg, None, 400)
+    subject = credential['credentialSubject']
+    if not 'roles' in subject:
+        msg = "No 'roles' in credentialSubject"
+        raise IssuerException(msg, None, 400)
+    token_roles = subject['roles']
+    if len(token_roles) < 1:
+        msg = "Empty 'roles' in credentialSubject"
+        raise IssuerException(msg, None, 400)
+
+    # Check for role
+    return check_role(token_roles, create_role, provider_id)
+
+# Forward request to TIL
+def forward_til_request(request, conf):
+
+    # Get URI of TIL service
+    if not 'tilUri' in conf['issuer']:
+        message = "Missing 'tilUri' in issuer config"
+        raise IssuerException("Internal server error", message, 500)
+    til_uri = conf['issuer']['tilUri']
+
+    # Build request parameters
+    headers = {k:v for k,v in request.headers if k != "Authorization" and k.lower() != 'host'}
+    url = request.url.replace(request.host_url, f'{til_uri}/')
+    data = request.get_data()
+    
+    # Forward request
+    response = requests.request(
+        method          = request.method,
+        url             = url,
+        headers         = headers,
+        data            = data,
+        cookies         = request.cookies,
+        allow_redirects = False,
+    )
+
+    return response
